4928b547d1e33a92d901bdca7f5da1cbf36586c9
Victor Gianvechio <victor.gianvecchio@live.com>
2019-11-06 15:37:38 -0300

    primeiro commit

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,40 @@
+/* eslint-disable class-methods-use-this */
+
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const student = await Student.create(req.body);
+    return res.json(student);
+  }
+
+  async update(req, res) {
+    const student = await Student.findByPk(req.params.id);
+
+    if (!student) {
+      res.status(401).json({ error: 'Student not found' });
+    }
+
+    // const studentExists = await Student.findOne({
+    //   where: { email: req.body.email },
+    // });
+
+    // if (studentExists) {
+    //   return res.status(400).json({ error: 'Student already exists' });
+    // }
+
+    await student.update(req.body);
+
+    return res.json(student);
+  }
+}
+
+export default new StudentController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'c772eab8788b9865ece64c1d6fcd8be3',
+  expiresIn: '7d',
+};
